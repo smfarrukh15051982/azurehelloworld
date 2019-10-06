4660435a16c11eca9f4d529c8ae4ddc150eabba7
Farrukh Akhtar <smfarrukh1982@gmail.com>
2019-10-07 01:36:43 +1000

    deployment greeting commit

--- HelloWorld/Controllers/HomeController.cs
+++ HelloWorld/Controllers/HomeController.cs
@@ -5,14 +5,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using HelloWorld.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace HelloWorld.Controllers
 {
     public class HomeController : Controller
     {
+        private IConfiguration configuration;
+
+        public HomeController(IConfiguration configuration) {
+            this.configuration = configuration;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var model = configuration["Greeting"];
+            return View("Index", model);
         }
 
         public IActionResult Privacy()
