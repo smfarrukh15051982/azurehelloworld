cb0b6fa6ffb00318c7c0b92fc34427b6431deae3
Farrukh Akhtar <smfarrukh1982@gmail.com>
2019-10-07 10:08:59 +1000

    update to use courses from cosmos

---
+++ HelloWorld/Controllers/CoursesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HelloWorld.Models;
+using HelloWorld.Services;
+
+namespace HelloWorld.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly CourseStore courseStore;
+
+        public CoursesController(CourseStore courseStore)
+        {
+            this.courseStore = courseStore;
+        }
+
+        public IActionResult Index()
+        {
+            var model = courseStore.GetAllCourses();
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Insert()
+        {
+            var data = new SampleData().GetCourses();
+            await courseStore.InsertCourses(data);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ HelloWorld/Services/CourseStore.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents.Client;
+using HelloWorld.Models;
+
+namespace HelloWorld.Services
+{
+    public class CourseStore
+    {
+        
+        private DocumentClient client;
+        private Uri coursesLink;
+
+        public CourseStore()
+        {
+          
+            var uri = new Uri("https://cosmoshelloworld.documents.azure.com:443/");
+            var key = "Aq3gCBspDwoARt1nCQnh6kQsDu3IBhSdWPouSwKyvgZb9Bk04ngULUX3A94JTIld90qRRNHrwSC1nGC4fE2jvw==";
+            client = new DocumentClient(uri, key);
+            coursesLink = UriFactory.CreateDocumentCollectionUri("pshelloazure", "courses");
+        }
+
+        public async Task InsertCourses(IEnumerable<Course> courses)
+        {
+            foreach(var course in courses)
+            {
+                await client.CreateDocumentAsync(coursesLink, course);
+            }
+        }
+
+        public IEnumerable<Course> GetAllCourses()
+        {
+            var courses = client.CreateDocumentQuery<Course>(coursesLink)
+                                .OrderBy(c => c.Title);
+
+            return courses;
+        }
+    }
+}
